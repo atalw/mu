d005dbdcb53edfbdd6600b42f6100659ebe006c9
Akshit Talwar <akshitalwar11@gmail.com>
2016-05-28 17:49:21 +0530

    Logged in user access to home page

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,27 @@ require('./../config/passport.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Playlister' });
+	// req.session.id = res;
+	res.render('login', { title: 'Playlister' });
 });
 
-router.get('/home', function(req, res, next) {
-	res.render('index', {title: 'Playlister'});
+router.get('/home', isAuthenticated, function(req, res, next) {
+	console.log(req.session.userId);
+	res.render('index', {title: req.session.userId});
 });
+
+function isAuthenticated(req, res, next) {
+	if(req.isAuthenticated()) {
+		req.session.userId = req.user.userId;
+		return next();
+	}
+	else {
+		res.redirect('/');
+	}
+}
 
 router.get('/auth/youtube', passport.authenticate('youtube', {scope: ['https://www.googleapis.com/auth/youtube'] }));
-		
+
 router.get('/auth/youtube/callback', passport.authenticate('youtube', {failureRedirect: '/'}),
 	function(req, res) {
 		res.redirect('/home');
