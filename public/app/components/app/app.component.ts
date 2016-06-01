d3343156d04e1830f73a6b01cd21c6531fba7143
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-01 23:38:19 +0530

    Configured Angular 2

---
+++ public/app/components/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { ContainerComponent } from '../container/container.component';
+
+@Component({
+  selector: 'my-app',
+  template: '<div class="container"></div>'
+})
+export class AppComponent {}
