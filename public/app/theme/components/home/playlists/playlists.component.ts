1e5ba2d5d7c3a5521eb0c8d394764833ff933811
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-06 00:09:48 +0530

    Added basic pagination using ng2-pagination

--- public/app/theme/components/home/playlists/playlists.component.ts
+++ public/app/theme/components/home/playlists/playlists.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import {PaginatePipe, PaginationControlsCmp, PaginationService} from 'ng2-pagination';
+
 
 @Component({
   selector: 'mu-playlists',
   templateUrl: 'app/theme/components/home/playlists/playlists.component.html',
+  directives: [PaginationControlsCmp],
+  pipes: [PaginatePipe],
+  providers: [PaginationService]
 })
 
 export class PlaylistsComponent {
-	size = ['1', '2', '3', '4'];
-
+	size = ['1', '2', '3', '4', '5', '6'];
 	item = {
 		song: "This",
 		artist: "is",
