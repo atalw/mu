ba4d3e786e9831145d17cad539cb128915ade5e6
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-08 18:37:23 +0530

    Added tabs component for separation of playlists; Tweaked styling

--- public/app/theme/components/home/home.component.ts
+++ public/app/theme/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { PlaylistsComponent } from './playlists/playlists.component';
+import { TabsComponent } from './tabs/tabs.component';
+
 
 
 @Component({
   selector: 'mu-home',
   templateUrl: 'app/theme/components/home/home.component.html',
-  directives: [PlaylistsComponent]
+  directives: [PlaylistsComponent, TabsComponent]
 })
 
 export class HomeComponent {}
---
+++ public/app/theme/components/home/tabs/tabs.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'mu-tabs',
+	templateUrl: 'app/theme/components/home/tabs/tabs.component.html'
+})
+
+export class TabsComponent {}
