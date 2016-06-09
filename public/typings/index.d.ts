2efb65a7cda17622e35963dcde8e6fb411cf1b52
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-09 18:48:47 +0530

    Added youtube declaration; tweaked ui

--- public/typings/index.d.ts
+++ public/typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/gapi.youtube/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
