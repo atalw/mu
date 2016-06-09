6b77313618f42b7f1deecdd0c520844f97749961
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-09 19:32:14 +0530

    Added gapi youtube declaration using typings

--- public/typings/index.d.ts
+++ public/typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/gapi.youtube/index.d.ts" />
+/// <reference path="globals/gapi/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
