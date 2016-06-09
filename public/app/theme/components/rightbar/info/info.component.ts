05c1733417da327267bfb0cd9ad821b5dcaf1ad8
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-09 14:03:55 +0530

    Added toggle button between related videos and lyrics; Removed ng2-pagination from config file

--- public/app/theme/components/rightbar/info/info.component.ts
+++ public/app/theme/components/rightbar/info/info.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgSwitch, NgSwitchWhen } from '@angular/common';
 import { RelatedVideosComponent } from './related-videos/related-videos.component';
 import { LyricsComponent } from './lyrics/lyrics.component';
 
@@ -6,7 +7,13 @@ import { LyricsComponent } from './lyrics/lyrics.component';
 @Component({
 	selector: 'mu-info',
 	templateUrl: 'app/theme/components/rightbar/info/info.component.html',
-	directives: [RelatedVideosComponent, LyricsComponent]
+	directives: [RelatedVideosComponent, LyricsComponent, NgSwitch, NgSwitchWhen]
 })
 
-export class InfoComponent {}
+export class InfoComponent {
+	tab = 'related-videos';
+
+	toggle() {
+		this.tab = this.tab === 'related-videos' ? 'lyrics' : 'related-videos';
+	}
+}
--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -9,14 +9,12 @@
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs',
-    'ng2-pagination':              'node_modules/ng2-pagination'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
-    'ng2-pagination':              { main: 'index.js', defaultExtension: 'js' }
   };
   var ngPackageNames = [
     'common',
