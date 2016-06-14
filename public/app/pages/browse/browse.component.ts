e7ffa44b3219069fb42069468b3bd059de5ecdf6
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-14 20:47:55 +0530

    Added artists component; tweaked theme

--- public/app/pages/browse/browse.component.ts
+++ public/app/pages/browse/browse.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { TopPlaylistsComponent } from './components/top-playlists/top-playlists.component';
 import { MoodComponent } from './components/mood/mood.component';
-
+import { ArtistsComponent } from './components/artists/artists.component'
 
 @Component({
   moduleId: module.id,
   selector: 'browse',
   templateUrl: 'browse.component.html',
-  directives: [TopPlaylistsComponent, MoodComponent]
+  directives: [TopPlaylistsComponent, MoodComponent, ArtistsComponent]
 })
 export class BrowseComponent {}
