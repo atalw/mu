8b92cee199835a2216105b1650cc28c0bb3d6c2d
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-17 12:13:24 +0530

    Moved toggle button inside lyrics/related-videos; Added minimise toggle button in right bar; Updated theme

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -12,34 +12,24 @@ import { RightbarComponent } from './theme/components/rightbar/rightbar.componen
   templateUrl: 'app.component.html',
   directives: [ROUTER_DIRECTIVES, NavbarComponent, ControlbarComponent, RightbarComponent],
   animations: [
-	  trigger('active', [
-		  state('void', style({ transform: 'translateX(0)' })),
-		  state('closed', style({
-			  transform: 'translateX(70%)',
-		  })),
-		  state('open', style({ transform: 'translateX(0)' })),
-		  transition('void => closed', [animate(0)]),
-		  transition('closed => open', [animate('500ms ease-out')]),
-		  transition('open => closed', [animate('500ms ease-out')])
-	  ]),
 	  trigger('routerEnlarge', [
 		  state('void', style({ transform: 'translateX(0)' })),
 		  state('closed', style({
-			  transform: 'translate(10%)',
+			  transform: 'translate(15%)',
 		  })),
 		  state('open', style({ transform: 'translateX(0)' })),
 		  transition('void => closed', [animate(0)]),
 		  transition('closed => open', [animate('500ms ease-out')]),
 		  transition('open => closed', [animate('500ms ease-out')])
 	  ])
-  ],
+  ]
 })
 
 export class AppComponent {
 
 	@Input() open = true;
 
-	onToggleRight() {
+	onToggleRight(event) {
 		this.open = !this.open;
 		let mainDiv = document.getElementById('main');
 		let rightDiv = document.getElementById('rightitis');
--- public/app/theme/components/rightbar/rightbar.component.ts
+++ public/app/theme/components/rightbar/rightbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, trigger, style, animate, state, transition, EventEmitter, Output } from '@angular/core';
 import { VideoPlayerComponent } from './videoPlayer/videoPlayer.component';
 import { InfoComponent } from './info/info.component';
 
@@ -6,7 +6,29 @@ import { InfoComponent } from './info/info.component';
   moduleId: module.id,
   selector: 'mu-right-bar',
   templateUrl: 'rightbar.component.html',
-  directives: [VideoPlayerComponent, InfoComponent]
+  directives: [VideoPlayerComponent, InfoComponent],
+  animations: [
+	  trigger('active', [
+		  state('void', style({ transform: 'translateX(0)' })),
+		  state('closed', style({
+			  transform: 'translateX(70%)',
+		  })),
+		  state('open', style({ transform: 'translateX(0)' })),
+		  transition('void => closed', [animate(0)]),
+		  transition('closed => open', [animate('500ms ease-out')]),
+		  transition('open => closed', [animate('500ms ease-out')])
+	  ])
+	]
 })
 
-export class RightbarComponent {}
+export class RightbarComponent {
+	@Input() open = true;
+	@Output() onToggle = new EventEmitter();
+
+	onToggleRight() {
+		this.open = !this.open;
+		this.onToggle.emit({
+			value: this.open
+		});
+	}
+}
