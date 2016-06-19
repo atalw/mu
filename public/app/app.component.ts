66c1436b9fdc9cb6550c7adadd38efdf508271d6
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-19 15:11:59 +0530

    Added and implemented material sidenav

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,45 +1,20 @@
 import { Component, Input, trigger, style, animate, state, transition, EventEmitter, Output } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
-import { NavbarComponent } from './theme/components/navbar/navbar.component';
+// import { NavbarComponent } from './theme/components/navbar/navbar.component';
 import { ControlbarComponent } from './theme/components/controlbar/controlbar.component';
-import { RightbarComponent } from './theme/components/rightbar/rightbar.component';
-
+// import { RightbarComponent } from './theme/components/rightbar/rightbar.component';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MdButton} from '@angular2-material/button';
+import { InfoComponent } from './theme/components/rightbar/info/info.component';
+import { VideoPlayerComponent } from './theme/components/rightbar/videoPlayer/videoPlayer.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app',
   templateUrl: 'app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ControlbarComponent, RightbarComponent],
-  animations: [
-	  trigger('routerEnlarge', [
-		  state('void', style({ transform: 'translateX(0)' })),
-		  state('closed', style({
-			  transform: 'translate(11%)',
-		  })),
-		  state('open', style({ transform: 'translateX(0)' })),
-		  transition('void => closed', [animate(0)]),
-		  transition('closed => open', [animate('350ms ease-out')]),
-		  transition('open => closed', [animate('350ms ease-out')])
-	  ])
-  ]
+  directives: [ROUTER_DIRECTIVES, ControlbarComponent, MD_SIDENAV_DIRECTIVES, MdButton, InfoComponent, VideoPlayerComponent],
+  animations: []
 })
 
-export class AppComponent {
-
-	@Input() open = true;
-
-	onToggleRight(event) {
-		this.open = !this.open;
-		let mainDiv = document.getElementById('main');
-		let rightDiv = document.getElementById('rightitis');
-		if (this.open === true) {
-			// mainDiv.className = "col-sm-8 padding-0";
-			// rightDiv.className = "col-sm-3 padding-0"
-		}
-		else {
-			// mainDiv.className = "col-sm-10 padding-0";
-			// rightDiv.className = "col-sm-1 padding-0"
-		}
-	}
-}
+export class AppComponent {}
--- public/app/theme/components/rightbar/rightbar.component.ts
+++ public/app/theme/components/rightbar/rightbar.component.ts
@@ -3,34 +3,12 @@ import { VideoPlayerComponent } from './videoPlayer/videoPlayer.component';
 import { InfoComponent } from './info/info.component';
 import {MdButton} from '@angular2-material/button';
 
-
 @Component({
   moduleId: module.id,
   selector: 'mu-right-bar',
   templateUrl: 'rightbar.component.html',
-  directives: [VideoPlayerComponent, InfoComponent, MdButton],
-  animations: [
-	  trigger('active', [
-		  state('void', style({ transform: 'translateX(0)' })),
-		  state('closed', style({
-			  transform: 'translateX(70%)',
-		  })),
-		  state('open', style({ transform: 'translateX(0)' })),
-		  transition('void => closed', [animate(0)]),
-		  transition('closed => open', [animate('350ms ease-out')]),
-		  transition('open => closed', [animate('350ms ease-out')])
-	  ])
-	]
+  // directives: [VideoPlayerComponent, InfoComponent, MdButton],
+  animations: []
 })
 
-export class RightbarComponent {
-	@Input() open = true;
-	@Output() onToggle = new EventEmitter();
-
-	onToggleRight() {
-		this.open = !this.open;
-		this.onToggle.emit({
-			value: this.open
-		});
-	}
-}
+export class RightbarComponent {}
