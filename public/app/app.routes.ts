dac2f74422604601aae1e990fdf53400764fa461
Akshit Talwar <akshitalwar11@gmail.com>
2016-06-26 21:20:57 +0800

    Added reddit browse component

--- public/app/app.routes.ts
+++ public/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './theme/components/home/home.component';
 import { RightbarComponent } from './theme/components/rightbar/rightbar.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BrowseComponent } from './pages/browse/browse.component';
+import { RedditComponent } from './pages/reddit/reddit.component';
 
 const routes: RouterConfig = [
 	{ path: '', component: HomeComponent},
 	{ path: 'login', component: LoginComponent },
-	{ path: 'browse', component: BrowseComponent }
+	{ path: 'browse', component: BrowseComponent },
+	{ path: 'reddit', component: RedditComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
---
+++ public/app/pages/reddit/reddit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'mu-reddit',
+	templateUrl: 'reddit.component.html',
+})
+export class RedditComponent {
+
+	constructor() { }
+
+}
