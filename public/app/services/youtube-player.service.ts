d659b71dca26d31612824f88c3519c27a3c96445
Akshit Talwar <akshitalwar11@gmail.com>
2016-07-03 10:49:19 +0530

    Added function to play/pause video from control bar

--- public/app/services/youtube-player.service.ts
+++ public/app/services/youtube-player.service.ts
@@ -4,21 +4,28 @@ import { Injectable } from '@angular/core';
 export class YoutubePlayerService {
 
 	public player;
+	// private isPlayerReady: boolean = false;
 	loadIframeAPI: Promise<any>;
 
 	constructor() {
+		this.initPlayer();
+		// this.mutePlayer();
+	}
+
+	initPlayer() {
 		this.loadIframeAPI = new Promise((resolve) => {
-			window['onYouTubeIframeAPIReady'] = () => {
-				if (window['YT']) {
-					this.player = new window.YT.Player('player', {
-						videoId: 'M7lc1UVf-VE'
-					});
-				}
-
-
+			window['onYouTubeIframeAPIReady'] = (ev) => {
+				this.player = new YT.Player('player', {
+					videoId: 'M7lc1UVf-VE',
+					events: {
+						'onReady': this.onPlayerReady,
+					}
+				});
+				console.log('player loaded')
+				resolve(window.player);
 			}
 			this.loadScript();
-		})
+		});
 	}
 
 	loadScript() {
@@ -26,5 +33,28 @@ export class YoutubePlayerService {
 		tag.src = "https://www.youtube.com/iframe_api";
 		var firstScriptTag = document.getElementsByTagName('script')[0];
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	}
+
+	onPlayerReady(event) {
+		// this.isPlayerReady = true;
+	}
+
+	playVideo() {
+		this.player.playVideo();
+	}
+
+	pauseVideo() {
+		this.player.pauseVideo();
+	}
+
+	getVideoDuration() {
+		return this.loadIframeAPI.then(()=> {
+			return this.player.getDuration();
+		})
+	}
+	getElapsedTime() {
+		return this.loadIframeAPI.then(() => {
+			return this.player.getCurrentTime();
+		})
 	}
 }
--- public/app/theme/components/controlbar/controlbar.component.ts
+++ public/app/theme/components/controlbar/controlbar.component.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import { YoutubePlayerService } from '../../../services/youtube-player.service';
 
 @Component({
   moduleId: module.id,
   selector: 'mu-control-bar',
   templateUrl: 'controlbar.component.html',
+  directives: [MdIcon],
+  providers: [YoutubePlayerService, MdIconRegistry]
 })
 
 export class ControlbarComponent {
 
-	pause() {
-		console.log('here');
+	private playerState;
+	private playOrPauseIcon;
+	private videoDuration;
+	private elapsedTime;
+
+	constructor(public youtubePlayerService : YoutubePlayerService) {
+		this.playOrPauseIcon = "play_arrow";
+
 	}
+
+	ngOnInit() {
+		this.elapsedTime = "00:00";
+		this.videoDuration = "00:00"
+		console.log(this.elapsedTime);
+	}
+
+	playOrPause() {
+		this.playerState = this.youtubePlayerService.player.getPlayerState();
+		if ( this.playerState == 1 || this.playerState == 3) {
+			this.youtubePlayerService.pauseVideo();
+			this.playOrPauseIcon = "play_arrow";
+		}
+
+		else {
+			this.youtubePlayerService.playVideo();
+			this.playOrPauseIcon = "pause";
+		}
+	}
+
+	getVideoDuration() {
+		var videoSeconds;
+		this.youtubePlayerService.getVideoDuration().then(response => {
+			videoSeconds = response;
+		})
+		var minutes = Math.floor(videoSeconds / 60);
+		var seconds = videoSeconds % 60;
+		this.videoDuration = minutes + ":" + seconds;
+		console.log(this.videoDuration);
+	}
+
 }
